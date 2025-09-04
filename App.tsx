e4d4b6fcebf2a7b9e2f2f90788a4253c92f5ea83
dragosb04 <dragosinfinit10@gmail.com>
2025-09-05 01:10:06 +0300

    Initialize application frontend

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
-import './global.css';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
-  return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
-  );
+    return (
+        <View style={styles.container}>
+            <MapView style={styles.map} />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+});
